b41de17ee239f74d0ddf50806b5a1baf2f9692fe
Robson Ramos <rpbramos@gmail.com>
2021-12-10 18:17:10 -0300

    added CNAME file

--- docs/build/_app/pages/docs/index.svelte-cd2a02db.js
+++
@@ -1,1 +0,0 @@
-import{S as e,i as o,s as n}from"../../chunks/vendor-05c14c71.js";function a({error:t,status:s}){return{status:301,redirect:"/docs/components/alert"}}class c extends e{constructor(s){super();o(this,s,null,null,n,{})}}export{c as default,a as load};
